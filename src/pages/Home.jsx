b2257fbabef26d61a12ab6a8040367294a613f17
Abdulrahman <remaxi135@gmail.com>
2025-08-06 07:51:25 +0100

    our

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
      return (
           <>
                <HeroSection
-                    heading={'Save Forest'}
+                    heading={'Save Our Forest'}
                     bg={"bg-hero_bg"} 
                     details={
                          ' Forests are vital to life on Earth — they clean our air, filter our water, and provide habitat to countless species. Every tree lost to deforestation is a step backward for climate and biodiversity. Let’s take action now.'
